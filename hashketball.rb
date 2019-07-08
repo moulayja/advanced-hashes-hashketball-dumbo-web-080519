c00930e86b4df352eae498439ccf733ff04198f9
Jamal Farah <moulayjam@gmail.com>
2019-07-08 17:34:45 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,143 +1,145 @@
-def game_hash
 
-  game_hash = {
-    home: {
-      team_name: "Brooklyn Nets",
-      colors: ["Black, White"],
-      players:{
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
         "Alan Anderson" => {
-          number: 0,
-          shoe: 16,
-          points: 22,
-          rebounds: 12,
-          assists: 12,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 1
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
         },
         "Reggie Evans" => {
-          number: 30,
-          shoe: 14,
-          points: 12,
-          rebounds: 12,
-          assists: 12,
-          steals: 12,
-          blocks: 12,
-          slam_dunks: 7
+        :number => 30,
+        :shoe => 14,
+        :points => 12,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 12,
+        :blocks => 12,
+        :slam_dunks => 7
         },
         "Brook Lopez" => {
-          number: 11,
-          shoe: 17,
-          points: 17,
-          rebounds: 19,
-          assists: 10,
-          steals: 3,
-          blocks: 1,
-          slam_dunks: 15
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assists => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 15
         },
         "Mason Plumlee" => {
-          number: 1,
-          shoe: 19,
-          points: 26,
-          rebounds: 12,
-          assists: 6,
-          steals: 3,
-          blocks: 8,
-          slam_dunks: 5
+        :number => 1,
+        :shoe => 19,
+        :points => 26,
+        :rebounds => 12,
+        :assists => 6,
+        :steals => 3,
+        :blocks => 8,
+        :slam_dunks => 5
         },
         "Jason Terry" => {
-          number: 31,
-          shoe: 15,
-          points: 19,
-          rebounds: 2,
-          assists: 2,
-          steals: 4,
-          blocks: 11,
-          slam_dunks: 1
+        :number => 31,
+        :shoe => 15,
+        :points => 19,
+        :rebounds => 2,
+        :assists => 2,
+        :steals => 4,
+        :blocks => 11,
+        :slam_dunks => 1
         }
       }
     },
-    away: {
-      team_name: "Charlotte Hornets",
-      colors: ["Turquoise, Purple"],
-      players: {
+    
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
         "Jeff Adrien" => {
-          number: 4,
-          shoe: 18,
-          points: 10,
-          rebounds: 1,
-          assists: 1,
-          steals: 2,
-          blocks: 7,
-          slam_dunks: 2
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assists => 1,
+        :steals => 2,
+        :blocks => 7,
+        :slam_dunks => 2
         },
         "Bismak Biyombo" => {
-          number: 0,
-          shoe: 16,
-          points: 12,
-          rebounds: 4,
-          assists: 7,
-          steals: 7,
-          blocks: 15,
-          slam_dunks: 10
+        :number => 0,
+        :shoe => 16,
+        :points => 12,
+        :rebounds => 4,
+        :assists => 7,
+        :steals => 7,
+        :blocks => 15,
+        :slam_dunks => 10
         },
         "DeSagna Diop" => {
-          number: 2,
-          shoe: 14,
-          points: 24,
-          rebounds: 12,
-          assists: 12,
-          steals: 4,
-          blocks: 5,
-          slam_dunks: 5
+        :number => 2,
+        :shoe => 14,
+        :points => 24,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 4,
+        :blocks => 5,
+        :slam_dunks => 5
         },
         "Ben Gordon" => {
-          number: 8,
-          shoe: 15,
-          points: 33,
-          rebounds: 3,
-          assists: 2,
-          steals: 1,
-          blocks: 1,
-          slam_dunks: 0
+        :number => 8,
+        :shoe => 15,
+        :points => 33,
+        :rebounds => 3,
+        :assists => 2,
+        :steals => 1,
+        :blocks => 1,
+        :slam_dunks => 0
         },
         "Brendan Haywood" => {
-          number: 33,
-          shoe: 15,
-          points: 6,
-          rebounds: 12,
-          assists: 12,
-          steals: 22,
-          blocks: 5,
-          slam_dunks: 12
-        }
+        :number => 33,
+        :shoe => 15,
+        :points => 6,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 22,
+        :blocks => 5,
+        :slam_dunks => 12
+        },
       }
     }
   }
-
 end
-
 def big_shoe_rebounds
-
-  big_shoe = nil
-  shoes = {}
-
-  game_hash.each do |location, data|
-    data.each do |key, value|
-      if key == :players
-        value.each do |name, stats|
-          stats.each do |stat, value|
-            shoes[name] = game_hash[location][:players][name][:shoe] if stat == :shoe
+  big_shoes = []
+  max_shoe = []
+  what_team = []
+  game_hash.each do |location, team_color_player|
+    team_color_player.each do |tcp, players|
+      if tcp == :players
+        players.each do |player, stat|
+          stat.each do |statistic, actual|
+            if statistic == :shoe
+              max_shoe.push(actual)
+              if max_shoe.max == actual
+                big_shoes.push(player)
+                what_team.push(location)
+              end
+            end
           end
         end
       end
     end
   end
-  
-  shoes.each {|player, size| big_shoe = player if size == shoes.values.max}
-  
-  big_shoe
-  
+  return game_hash[what_team.last][:players][big_shoes.last][:rebounds]
 end
 
 big_shoe_rebounds
+
+
